5fc134ec90342e7c010a22bf4c9c8187f07b1abd
Kattamuri, Manikanta <Manikanta.Kattamuri@cdk.com>
2017-05-25 15:40:15 +0530

    removed all unused libraries

--- ZooWebApp/App_Start/BundleConfig.cs
+++ ZooWebApp/App_Start/BundleConfig.cs
@@ -12,27 +12,15 @@ namespace ZooWebApp
                         "~/Scripts/libraries/jquery-{version}.js"));
 
 
-            bundles.Add(new ScriptBundle("~/bundles/unobstrusive").Include(
-                      "~/Scripts/libraries/jquery.validate.js",
-                      "~/Scripts/libraries/jquery.validate.unobtrusive.js"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/libraries/modernizr-*"));
-
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/libraries/bootstrap.js",
-                      "~/Scripts/libraries/respond.js"));
+                      "~/Scripts/libraries/bootstrap.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/toastr").Include(
-                      "~/Scripts/libraries/toastr.js",
-                      "~/Scripts/libraries/respond.js"));
+                      "~/Scripts/libraries/toastr.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/angular").Include(
                      "~/Scripts/libraries/angular.min.js",
                      "~/Scripts/libraries/angular-route.min.js"));
-
                 
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
